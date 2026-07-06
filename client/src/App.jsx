a55639fa213470346fdf6db4b0dccfa34976e5cb
Yunita <dyunita839@gmail.com>
2026-07-06 08:12:31 +0700

    newest

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
@@ -109,36 +108,4 @@ export default function App() {
       </Routes>
     </BrowserRouter>
   );                                                                          
-=======
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// Landing Page
-import LandingPage from "./pages/LandingPage";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-
-// Dashboard
-import AdminDashboard from "./pages/AdminDashboard";
-import ManagerDashboard from "./pages/ManagerDashboard";
-import UserDashboard from "./pages/UserDashboard";
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* Landing Page */}
-        <Route path="/" element={<LandingPage />} />
-
-        {/* Auth */}
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-
-        {/* Dashboard */}
-        <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/manager" element={<ManagerDashboard />} />
-        <Route path="/user" element={<UserDashboard />} />
-      </Routes>
-    </BrowserRouter>
-  );                                                                        
->>>>>>> 04a7edbcff2dfc95e437a8dd4169afd100df5539
 }
