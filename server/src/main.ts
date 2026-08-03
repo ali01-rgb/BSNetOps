436a677873958c62a070625067022ae1466d592f
ali01-rgb <rohmanmuhali@gmail.com>
2026-08-04 00:10:56 +0700

    Kun Fayakun, Allahu Akbar

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,17 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // 🔥 BUKA GEMBOK CORS DI SINI 🔥
+  // 🔥 JURUS SAPU JAGAT CORS 🔥
   app.enableCors({
-    origin: [
-      'https://bsnetops.vercel.app', // Domain Vercel kamu (JANGAN ADA TANDA '/' DI AKHIR)
-      'http://localhost:5173',       // Izin untuk testing lokal di laptopmu
-    ],
+    origin: true, // <-- Ubah bagian ini jadi 'true' 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
-  // Gunakan port dari environment Railway atau fallback ke 3000
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
